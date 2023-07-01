f8efe17ecc934101432dac7b4f9b765b8d092f45
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2023-06-30 22:50:12 -0400

    adding 2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicialComponent,
+    SegundoComponenteComponent
   ],
   imports: [
     BrowserModule,
