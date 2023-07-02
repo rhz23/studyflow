e74d8dfad555472926f9b42184be05ba1765a66f
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2023-07-02 19:06:27 -0400

    atividade-08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { BooksCardComponent } from './books-card/books-card.component';
+import { ProjectsCardComponent } from './projects-card/projects-card.component';
+import { TodoCardComponent } from './todo-card/todo-card.component';
+import { LandPageComponent } from './land-page/land-page.component';
+import { CadastroBookComponent } from './cadastro-book/cadastro-book.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaginaInicialComponent,
-    SegundoComponenteComponent
+    SegundoComponenteComponent,
+    MenuBarComponent,
+    BooksCardComponent,
+    ProjectsCardComponent,
+    TodoCardComponent,
+    LandPageComponent,
+    CadastroBookComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
